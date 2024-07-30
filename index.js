71a3ee38b0505a0b6107fbf888aa87eaf23ef55e
Gabriel DP <gabrieldominguezprado@gmail.com>
2024-07-30 01:37:18 -0300

    Despliegue en Render

--- index.js
+++ index.js
@@ -26,14 +26,6 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
-// Manejar demás rutas
-
-app.get('*', (req, res) => {
-    
-    res.sendFile( path.resolve('./public/index.html') );
-});
-
-
 app.listen( process.env.PORT, () => {
     
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
